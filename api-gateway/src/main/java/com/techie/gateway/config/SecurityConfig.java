8e3d08403183efa1464e3e3609f31af03467e299
Jiya Rana <ranajiya563@gmail.com>
2025-09-10 22:45:00 +0530

    Initial commit

---
+++ api-gateway/src/main/java/com/techie/gateway/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.techie.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+                "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(Customizer.withDefaults())   // ✅ Enable CORS with the bean below
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls)
+                        .permitAll()
+                        .anyRequest()
+                            .authenticated())
+                        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                        .build();
+    }
+
+    // ✅ CORS rules
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+insert into t_inventory (quantity, sku_code)
+value (100, 'iphone_15'),
+      (100, 'pixel_8'),
+      (100, 'galaxy_24'),
+      (100, 'oneplus_12');
